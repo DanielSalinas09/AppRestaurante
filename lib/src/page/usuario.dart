3bf12d5ce524a63d5eaaf5e3bffedaa145d4f4fd
lobbito <wishollow@gmail.com>
2021-04-29 14:34:34 -0500

    responsive usuario

--- lib/src/page/usuario.dart
+++ lib/src/page/usuario.dart
@@ -12,12 +12,48 @@ class Usuario extends StatefulWidget {
 class _UsuarioState extends State<Usuario> {
   @override
   Widget build(BuildContext context) {
+    final _sizedScreen = MediaQuery.of(context).size;
     final infoProvider = Provider.of<InfoProvider>(context);
     return Scaffold(
       backgroundColor: Color(0xEEEEEEEEEE),
-      body: Column(
+      body: ListView(children: [
+          Column(
         children: [
-          Container(
+          _logo(infoProvider.nombre, infoProvider.apellido),
+          Column(
+            children: [
+              Container(
+                width: _sizedScreen.width * 0.7,
+                child: _historialPedidos(),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Container(
+                width:_sizedScreen.width * 0.7,
+                child:_configuracion()
+                ),          
+            ],
+          ),
+          Column(
+            children: [
+              Container(width:_sizedScreen.width * 0.7,
+               child:_cerrarSesion() 
+               ),
+              
+            ],
+          ),
+        ],
+      ),
+      ],
+      )
+      
+    );
+  }
+  _logo(String nombre, String apellido){
+    return
+Container(
             decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
@@ -39,115 +75,128 @@ class _UsuarioState extends State<Usuario> {
                 )),
                 Center(
                     child: Text(
-                        infoProvider.nombre + " " + infoProvider.apellido,
+                        nombre + " " + apellido,
                         style:
                             TextStyle(fontFamily: 'Chonburi', fontSize: 20))),
               ],
             ),
-          ),
-          Column(
-            children: [
-              Row(
+          );
+  }
+  _historialPedidos(){
+    return
+    Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  InkWell(
-                    child: Container(
-                      padding: EdgeInsets.all(7.0),
-                      margin: EdgeInsets.all(20.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                        color: Colors.white,
+                  Expanded(
+                      child: InkWell(
+                      child: Container(
+                        padding: EdgeInsets.all(7.0),
+                        margin: EdgeInsets.all(20.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                          color: Colors.white,
+                        ),
+                        child: Row(
+                          children: [
+                            Expanded(
+                                child: Text(
+                                'Historial de pedidos',
+                                style: TextStyle(
+                                    fontSize: 18.0, fontFamily: 'Chonburi'),
+                              ),
+                            ),
+                            IconButton(
+                                    alignment: Alignment.bottomRight,
+                                icon: Icon(Icons.arrow_forward),
+                                onPressed: () {},
+                              ),
+                            
+                          ],
+                        ),
                       ),
-                      child: Row(
-                        children: [
-                          Text(
-                            'Historial de pedidos ',
-                            style: TextStyle(
-                                fontSize: 18.0, fontFamily: 'Chonburi'),
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.arrow_forward),
-                            onPressed: () {},
-                          )
-                        ],
-                      ),
+                      onTap: () =>
+                          {Navigator.pushNamed(context, 'historialPedidos')},
                     ),
-                    onTap: () =>
-                        {Navigator.pushNamed(context, 'historialPedidos')},
                   ),
                 ],
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              Row(
+              );
+  }
+  _configuracion(){
+    return
+    Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  InkWell(
-                    child: Container(
-                      padding: EdgeInsets.all(7.0),
-                      margin: EdgeInsets.all(20.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                        color: Colors.white,
+                  Expanded(
+                                      child: InkWell(
+                      child: Container(
+                        padding: EdgeInsets.all(7.0),
+                        margin: EdgeInsets.all(20.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                          color: Colors.white,
+                        ),
+                        child: Row(
+                          children: [
+                            Expanded(
+                                child: Text(
+                                'Configuracion',
+                                style: TextStyle(
+                                    fontSize: 18.0, fontFamily: 'Chonburi'),
+                              ),
+                            ),
+                             IconButton(
+                                    alignment: Alignment.bottomRight,
+                                icon: Icon(Icons.arrow_forward),
+                                onPressed: () {},
+                              ),
+                            
+                          ],
+                        ),
                       ),
-                      child: Row(
-                        children: [
-                          Text(
-                            'Configuracion             ',
-                            style: TextStyle(
-                                fontSize: 18.0, fontFamily: 'Chonburi'),
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.arrow_forward),
-                            onPressed: () {},
-                          )
-                        ],
-                      ),
+                      onTap: () =>
+                          {Navigator.pushNamed(context, 'configuracion')},
                     ),
-                    onTap: () =>
-                        {Navigator.pushNamed(context, 'configuracion')},
                   ),
                 ],
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              Row(
+              );
+  }
+  _cerrarSesion(){
+return
+Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  InkWell(
-                    child: Container(
-                      padding: EdgeInsets.all(7.0),
-                      margin: EdgeInsets.all(20.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                        color: Colors.white,
+                  Expanded(
+                       child: InkWell(
+                      child: Container(
+                        padding: EdgeInsets.all(7.0),
+                        margin: EdgeInsets.all(20.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                          color: Colors.white,
+                        ),
+                        child: Row(
+                          children: [
+                            Expanded(
+                                child: Text(
+                                'Cerrar sesión',
+                                style: TextStyle(
+                                    fontSize: 18.0, fontFamily: 'Chonburi'),
+                              ),
+                            ),
+                            
+                                 IconButton(
+                                  alignment: Alignment.bottomRight,
+                                icon: Icon(Icons.arrow_forward),
+                                onPressed: () {},
+                              ),
+                            
+                          ],
+                        ),
                       ),
-                      child: Row(
-                        children: [
-                          Text(
-                            'Cerrar sesión              ',
-                            style: TextStyle(
-                                fontSize: 18.0, fontFamily: 'Chonburi'),
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.arrow_forward),
-                            onPressed: () {},
-                          )
-                        ],
-                      ),
+                      onTap: () => {Navigator.pushNamed(context, 'login')},
                     ),
-                    onTap: () => {Navigator.pushNamed(context, 'login')},
                   ),
                 ],
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+              );
   }
 }
